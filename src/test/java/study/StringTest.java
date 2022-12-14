784619f7f67bb7096121e702cad218471bc98e88
grasshopper42 <solite4686@gmail.com>
2022-12-14 14:40:09 +0900

    0. 학습테스트 실습 - StringTest 구현

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,42 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    private String[] splitWithComma(String str) {
+        return str.split(",");
+    }
+
+    @Test
+    void split() {
+        String[] actual1 = splitWithComma("1,2");
+        String[] actual2 = splitWithComma("1");
+        assertThat(actual1).contains("1", "2");
+        assertThat(actual2).containsExactly("1");
+    }
+
+    @Test
+    void substring() {
+        String actual = "(1,2)".substring(1, 4);
+        assertThat(actual).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("charAt 메소드 테스트")
+    void charAt() {
+        char actual = "abc".charAt(1);
+        assertThat(actual).isEqualTo('b');
+
+        assertThatThrownBy(() -> "abc".charAt(4)).isInstanceOf(IndexOutOfBoundsException.class);
     }
 }
